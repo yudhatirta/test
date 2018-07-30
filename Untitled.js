d5053eadd7d34506cbc531dc10302312f5227b48
Yudha Tirtandanu <yudha.tirtandanu@traveloka.com>
2018-07-30 18:57:15 +0700

    test

---
+++ Untitled.js
@@ -0,0 +1,21 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf400
+{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fmodern\fcharset0 Courier-Oblique;}
+{\colortbl;\red255\green255\blue255;\red129\green48\blue25;\red246\green246\blue255;\red31\green129\blue78;
+\red0\green0\blue0;\red51\green110\blue109;}
+{\*\expandedcolortbl;;\cssrgb\c58431\c25490\c12941;\cssrgb\c97255\c97255\c100000;\cssrgb\c12941\c56863\c38039;
+\cssrgb\c0\c0\c0;\cssrgb\c25098\c50196\c50196;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+import\cf4  \{ Selector \} \cf2 from\cf4  'testcafe';\
+\
+fixture `Getting Started`\
+    .page `http://devexpress.github.io/testcafe/example`;\
+\
+test('My first test', \cf2 async\cf4  t => \{\
+    
+\f1\i \cf6 // Test code
+\f0\i0 \cf4 \
+\});}
